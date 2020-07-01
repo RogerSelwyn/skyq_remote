8e33c5e343b979df34c22aac3a37e5072b137f5f
RogerSelwyn <RogerSelwyn@users.noreply.github.com>
2020-07-01 11:56:19 +0100

    Update generate_releasenotes.py

--- manage/generate_releasenotes.py
+++ manage/generate_releasenotes.py
@@ -113,6 +113,7 @@ if UPDATERELEASE == "yes":
             ),
             repository=REPOSITORY
         ),
+        prelease=True,
     )
 else:
     repo_changes = get_repo_commits(GITHUB, False)
